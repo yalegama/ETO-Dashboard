5552ffaf9d45020c411c971ff9060c2e39957bb6
Yalegama <nadunyalegama@.com>
2022-02-27 16:42:53 +0530

    setup the json types

--- client/src/examples/FileUpload/FileUpload.jsx
+++ client/src/examples/FileUpload/FileUpload.jsx
@@ -1,83 +1,8 @@
-import React, { useState } from 'react'
-import "./FileUpload.css"
-import * as XLSX from 'xlsx';
+import React from 'react'
 
 function FileUpload() {
-  //onchange
-  const [excelFile, setexcelFile] = useState(null);
-  const [excelFileError, setexcelFileError] = useState(null);
-
-  //excel data
-  //it will be contain array of object
-  const [excelData, setexcelData] = useState(null)
-  
-  const fileType=['application/vnd.ms-excel']
-  const inputFile=(e)=>{
-    let selectedFile=e.target.files[0];
-    if(selectedFile){
-
-      if(selectedFile&&fileType.includes(selectedFile.type)){
-        let reader=new FileReader();
-        reader.readAsArrayBuffer(selectedFile);
-        reader.onload=(e)=>{
-          setexcelFileError(null);
-          setexcelFile(e.target.result);
-        }
-      }else{
-
-        setexcelFileError("Please Select only Excel File Type");
-        setexcelFile(null)
-      }
-    }else{
-      console.log("Please Select Your File")
-    }
-  }
-
-  const handleSubmit=(e)=>{
-    e.preventDefault();
-    if(excelFile!==null){
-      const workbook=XLSX.read(excelFile,{type:'buffer'});
-      
-      //etoSummary
-      const etoSummary=workbook.SheetNames[0];
-      const etoSummaryWorkSheet=workbook.Sheets[etoSummary];
-      const etoSummaryData=XLSX.utils.sheet_to_json(etoSummaryWorkSheet);
-    
-      //absent Reasons
-      const absentReasons=workbook.SheetNames[1];
-      const absentReasonsWorkSheet=workbook.Sheets[absentReasons];
-      const absentReasonData=XLSX.utils.sheet_to_json(absentReasonsWorkSheet);
-
-      //Eto REASONS
-
-      const etoReasons=workbook.SheetNames[3];
-      const etoReasonsWorkSheet=workbook.Sheets[etoReasons];
-      const etoReasonData=XLSX.utils.sheet_to_json(etoReasonsWorkSheet);
-
-
-      //absent summary
-      const absentSummary=workbook.SheetNames[4];
-      const absentSummaryWorkSheet=workbook.Sheets[absentSummary];
-      const absentSummaryData=XLSX.utils.sheet_to_json(absentSummaryWorkSheet);
-      console.log(etoSummaryData)
-      console.log(absentReasonData)
-      console.log(etoReasonData);
-      console.log(absentSummaryData)
-    }else{
-      setexcelData(null);
-    }
-  }
-  
   return (
-    <div>
-      <div>
-        <form action="" type="Submit" onSubmit={handleSubmit}>
-          <label className='label' htmlFor="">FileUpload</label>
-          <input className='file' type="file" onChange={inputFile} />
-          <button className='button'>Submit</button>
-        </form>
-      </div>
-    </div>
+    <div>FileUpload</div>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,7 +1,50 @@
 const express=require("express");
 const app=express();
 const PORT=process.env.PORT || 3001;
+const mysql=require('mysql');
+const cors=require('cors');
 
+app.use(cors());
+app.use(express.json());
+
+const db=mysql.createConnection({
+    user:"root",
+    host:'localhost',
+    password:'',
+    database:'erdashboard'
+})
+
+app.post("/etoSummary",(req,res)=>{
+    const date=req.body.date;
+    const area=req.body.area;
+    const intenalCarder=req.body.intenalCarder;
+    const actualcarder=req.body.actualcarder;
+    const vop=req.body.vop;
+    const resign=req.body.resign;
+    const total=req.body.total;
+    const voppercentage=req.body.voppercentage;
+    const eto=req.body.eto;
+    const areaLeader=req.body.areaLeader;
+    const shift=req.body.shift;
+    const vsl=req.body.vsl;
+    const er=req.body.er;
+    const gl=req.body.gl;
+    const godFather=req.body.godFather;
+    const lokuAkka=req.body.lokuAkka;
+
+    db.query(
+        "INSERT INTO etosummary (date,area,intenalCarder,actualcarder,vop,resign,total,voppercentage,eto,areaLeader,shift,vsl,er,gl,godFather,lokuAkka) VALUES ('?','?','?','?','?','?','?','?','?','?','?','?','?','?','?','?')",
+        [date,area,intenalCarder,actualcarder,vop,resign,total,voppercentage,eto,areaLeader,shift,vsl,er,gl,godFather,lokuAkka],
+        (err,result)=>{
+            if(err){
+                console.log(err)
+            }else{
+                res.send("Values Inserted")
+            }
+        }
+    )
+
+})
 
 app.listen(PORT,()=>{
     console.log(`Server is started on ${PORT} port`);
