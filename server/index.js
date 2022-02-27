665930b2e5550d8cc83f9c2f6c5accea8445f3d5
Yalegama <nadunyalegama@.com>
2022-02-27 13:56:26 +0530

    created backend server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,8 @@
-const express=require("e")
+const express=require("express");
+const app=express();
+const PORT=process.env.PORT || 3001;
+
+
+app.listen(PORT,()=>{
+    console.log(`Server is started on ${PORT} port`);
+})
