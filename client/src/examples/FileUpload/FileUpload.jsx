f33191ab3e8c71d49c3d7e2d2cb55171077b9698
Yalegama <nadunyalegama@.com>
2022-02-28 07:25:27 +0530

    edited

--- client/src/examples/FileUpload/FileUpload.jsx
+++ client/src/examples/FileUpload/FileUpload.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import "./FileUpload.css"
+import * as XLSX from 'xlsx';
+import data from 'layouts/dashboard/components/Projects/data';
 
 function FileUpload() {
+
+  const readExcel=(file)=>{
+
+    const promise=new Promise((resolve,reject)=>{
+      const file=new FileReader();
+      FileReader.readAsArrayBuffer(file);
+      FileReader.onload=(e)=>{
+        const bufferArray=e.target.result;
+        const wb=XLSX.read(bufferArray,{type:'buffer'});
+        const wsname=wb.SheetNames[0];
+        const ws=wb.Sheets[wsname];
+        const data=XLSX.utils.sheet_to_json(ws);
+        resolve(data);
+      };
+      FileReader.onerror=(error)=>{
+        reject(error);
+      }
+    });
+    promise.then((d)=>{
+      console.log(data)
+    })
+
+  }
+
   return (
-    <div>FileUpload</div>
+    <div>
+      <div>
+        <form action="" type="Submit" >
+          <label className='label' htmlFor="">FileUpload</label>
+          <input className='file' type="file" onChange={(e)=>{
+            const file=e.target.files[0];
+            readExcel(file);
+          }} />
+          <button className='button'>Submit</button>
+        </form>
+      </div>
+    </div>
   )
 }
 
